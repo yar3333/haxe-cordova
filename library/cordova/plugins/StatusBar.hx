0b878f7efb9853cf0fd92f66b94b7d9ac5950da1
Yaroslav Sivakov <yar3333@gmail.com>
2015-12-02 20:02:11 +0300

    * conversion from TypeScript v0.7.9

--- library/cordova/plugins/StatusBar.hx
+++ library/cordova/plugins/StatusBar.hx
@@ -4,44 +4,37 @@ import js.*;
 import js.html.*;
 
 /**
-* Global object StatusBar.
-*/
-extern class Window
-{
-	var StatusBar : StatusBar;
-}
-
-/**
 * The StatusBar object provides some functions to customize the iOS and Android StatusBar.
 */
-typedef StatusBar =
+@:noUsing
+@:native("window.StatusBar") extern class StatusBar
 {
 	/**
 	* On iOS 7, make the statusbar overlay or not overlay the WebView.
 	* @param isOverlay         On iOS 7, set to false to make the statusbar appear like iOS 6.
 	*                              Set the style and background color to suit using the other functions.
 	*/
-	var overlaysWebView : Bool->Void;
+	static var overlaysWebView : Bool->Void;
 
 	/**
 	* Use the default statusbar (dark text, for light backgrounds).
 	*/
-	var styleDefault : Void->Void;
+	static var styleDefault : Void->Void;
 
 	/**
 	* Use the lightContent statusbar (light text, for dark backgrounds).
 	*/
-	var styleLightContent : Void->Void;
+	static var styleLightContent : Void->Void;
 
 	/**
 	* Use the blackTranslucent statusbar (light text, for dark backgrounds).
 	*/
-	var styleBlackTranslucent : Void->Void;
+	static var styleBlackTranslucent : Void->Void;
 
 	/**
 	* Use the blackOpaque statusbar (light text, for dark backgrounds).
 	*/
-	var styleBlackOpaque : Void->Void;
+	static var styleBlackOpaque : Void->Void;
 
 	/**
 	* On iOS 7, when you set StatusBar.statusBarOverlaysWebView to false,
@@ -49,7 +42,7 @@ typedef StatusBar =
 	* @param color             Supported color names are:
 	*                              black, darkGray, lightGray, white, gray, red, green, blue, cyan, yellow, magenta, orange, purple, brown
 	*/
-	var backgroundColorByName : String->Void;
+	static var backgroundColorByName : String->Void;
 
 	/**
 	* Sets the background color of the statusbar by a hex string.
@@ -57,20 +50,20 @@ typedef StatusBar =
 	*                              On iOS 7, when you set StatusBar.statusBarOverlaysWebView to false, you can set the background color of the statusbar by a hex string (#RRGGBB).
 	*                              On WP7 and WP8 you can also specify values as #AARRGGBB, where AA is an alpha value
 	*/
-	var backgroundColorByHexString : String->Void;
+	static var backgroundColorByHexString : String->Void;
 
 	/**
 	* Hide the statusbar.
 	*/
-	var hide : Void->Void;
+	static var hide : Void->Void;
 
 	/**
 	* Show the statusbar.
 	*/
-	var show : Void->Void;
+	static var show : Void->Void;
 
 	/**
 	* Read this property to see if the statusbar is visible or not.
 	*/
-	var isVisible : Bool;
+	static var isVisible : Bool;
 }
